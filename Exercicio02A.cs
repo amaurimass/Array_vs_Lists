20aa84200fc30a7629664abf5c7c43189658f8e5
Amauri Marques Assunção Silva <127672828+amaurimass@users.noreply.github.com>
2023-03-12 12:30:26 -0300

    Add files via upload

---
+++ Exercicio02A.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02_Arrays_vs_Lists
+{
+    public class Exercicio02A
+    {
+        //Desenvolvimento de um programa que armazena uma lista de convidados utilizando um array.
+
+        public static void Main()
+
+        {
+            string[] convidados = new string[10];
+
+            convidados[0] = "Antonio";
+            convidados[1] = "Debora";
+            convidados[2] = "Tamires";
+            convidados[3] = "Arthur";
+            convidados[4] = "Henrique";
+            convidados[5] = "Amauri";
+            convidados[6] = "Kelly;";
+            convidados[7] = "Flavio";
+            convidados[8] = "Karina";
+            convidados[9] = "Nathalia";
+
+            Console.WriteLine("O número de convidados é igual a " + convidados.Length);
+        }
+
+    }
+
+              
+}
---
+++ Exercicio02B.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//Desenvolvimento de outra versao do programa de lista de convidados, agora no formato de lista.
+
+public class Exercicio02B
+{
+	public static void main ()
+
+	{
+        List<string> convidados = new List<string>();
+        convidados.Add("Antonio");
+        convidados.Add("Debora");
+        convidados.Add("Tamires");
+        convidados.Add("Arthur");
+        convidados.Add("Henrique");
+        convidados.Add("Amauri");
+        convidados.Add("Kelly");
+        convidados.Add("Flavio");
+        convidados.Add("Karina");
+        convidados.Add("Nathalia");
+
+        Console.WriteLine("O número de convidados na lista é de: " + convidados.Count);
+        Console.WriteLine("Convidado: " + convidados[0]);
+        Console.WriteLine("Convidado: " + convidados[1]);
+        Console.WriteLine("Convidado: " + convidados[2]);
+        Console.WriteLine("Convidado: " + convidados[3]);
+        Console.WriteLine("Convidado: " + convidados[4]);
+        Console.WriteLine("Convidado: " + convidados[5]);
+        Console.WriteLine("Convidado: " + convidados[6]);
+        Console.WriteLine("Convidado: " + convidados[7]);
+        Console.WriteLine("Convidado: " + convidados[8]);
+        Console.WriteLine("Convidado: " + convidados[9]);
+        Console.WriteLine("Convidado: " + convidados[10]);
+        Console.WriteLine("Convidado: " + convidados[11]);
+    }
+}
